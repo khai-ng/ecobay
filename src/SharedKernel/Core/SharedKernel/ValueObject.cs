658121bd49bb348b62bdec884fe87bee4d7476a2
khai-nq <khai.nguyen.quang@gosell.vn>
2024-07-21 18:12:45 +0700

    add: ValueObject class

---
+++ src/SharedKernel/Core/SharedKernel/ValueObject.cs
@@ -0,0 +1,39 @@
+﻿namespace Core.SharedKernel
+{
+    //Reference: EShopContainer by Microsoft
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+
+        public static bool operator ==(ValueObject? left, ValueObject? right)
+        {
+            if(left is null && right is null)
+                return true;
+
+            if (left is null || right is null)
+                return false;
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ValueObject? left, ValueObject? right) => !(left == right);
+
+        public bool Equals(ValueObject? other)
+        {
+            if (other == null) return false;
+
+            return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public override bool Equals(object obj) =>
+            obj is ValueObject valueObject && Equals(valueObject);
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x != null ? x.GetHashCode() : 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+    }
+}
