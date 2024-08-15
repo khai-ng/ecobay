4c851b66845ddef9595274dcb3888c231a916575
khai-nq <khai.nguyen.quang@gosell.vn>
2024-08-15 10:02:36 +0700

    chore(results): add comment

--- src/SharedKernel/Core/Results/AppResults/AppResult.cs
+++ src/SharedKernel/Core/Results/AppResults/AppResult.cs
@@ -31,62 +31,118 @@ namespace Core.Result.AppResults
             return new AppResult<T>(value);
         }
 
-        public static implicit operator AppResult<T>(AppResult result) => new AppResult<T>(default(T))
+        public static implicit operator AppResult<T>(AppResult result) => new(default(T))
         {
             Status = result.Status,
             Errors = result.Errors,
         };
+
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Ok"/>, equivalent <seealso cref="HttpStatusCode.OK"/><br/>
+        /// Indicates that the request has succeeded
+        /// </summary>
+        /// <returns></returns>
 
         public static AppResult<T> Success(T value)
         {
             return new AppResult<T>(value);
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Error"/>, equivalent <seealso cref="HttpStatusCode.InternalServerError"/><br/>
+        /// Indicates that the server has occur unexpected error
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> Error(string message)
         {
             return new AppResult<T>(AppStatusCode.Error) { Message = message };
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Error"/>, equivalent <seealso cref="HttpStatusCode.InternalServerError"/><br/>
+        /// Indicates that the server has occur unexpected error
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> Error(params string[] errorDetailMessages)
         {
             return new AppResult<T>(AppStatusCode.Error) { Errors = errorDetailMessages.Select(e => new ErrorDetail(e)) };
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Invalid"/>, equivalent <seealso cref="HttpStatusCode.BadRequest"/><br/>
+        /// Indicates that the request could not be understood by the server
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> Invalid(ErrorDetail error)
         {
             return new AppResult<T>(AppStatusCode.Invalid) { Errors = new List<ErrorDetail>() { error } };
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Invalid"/>, equivalent <seealso cref="HttpStatusCode.BadRequest"/><br/>
+        /// Indicates that the request could not be understood by the server
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> Invalid(params ErrorDetail[] errors)
         {
             return new AppResult<T>(AppStatusCode.Invalid) { Errors = errors };
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Invalid"/>, equivalent <seealso cref="HttpStatusCode.BadRequest"/><br/>
+        /// Indicates that the request could not be understood by the server
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> Invalid(IEnumerable<ErrorDetail> errors)
         {
             return new AppResult<T>(AppStatusCode.Invalid) { Errors = errors };
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.NotFound"/>, equivalent <seealso cref="HttpStatusCode.NotFound"/><br/>
+        /// Indicates that the server did not find a current representation for the target resource
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> NotFound()
         {
             return new AppResult<T>(AppStatusCode.NotFound);
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.NotFound"/>, equivalent <seealso cref="HttpStatusCode.NotFound"/><br/>
+        /// Indicates that the server did not find a current representation for the target resource
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> NotFound(string message)
         {
             return new AppResult<T>(AppStatusCode.NotFound) { Message = message };
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.NotFound"/>, equivalent <seealso cref="HttpStatusCode.NotFound"/><br/>
+        /// Indicates that the server did not find a current representation for the target resource
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> NotFound(params string[] errorDetailMessages)
         {
             return new AppResult<T>(AppStatusCode.NotFound) { Errors = errorDetailMessages.Select(e => new ErrorDetail(e)) };
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Forbidden"/>, equivalent <seealso cref="HttpStatusCode.Forbidden"/><br/>
+        /// Indicates that the server understood the request but refuses to authorize
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> Forbidden()
         {
             return new AppResult<T>(AppStatusCode.Forbidden);
         }
 
+        /// <summary>
+        /// Creates result with status <see cref="AppStatusCode.Unauthorized"/>, equivalent <seealso cref="HttpStatusCode.Unauthorized"/><br/>
+        /// Indicates that the request requires authorization
+        /// </summary>
+        /// <returns></returns>
         public static AppResult<T> Unauthorized()
         {
             return new AppResult<T>(AppStatusCode.Unauthorized);
--- src/SharedKernel/Core/Results/AppResults/AppStatusCode.cs
+++ src/SharedKernel/Core/Results/AppResults/AppStatusCode.cs
@@ -2,13 +2,37 @@
 {
     public enum AppStatusCode
     {
+        /// <summary>
+        /// Indicates that the request has succeeded
+        /// </summary>
         Ok,
+        /// <summary>
+        /// Indicates that the server has occur unexpected error 
+        /// </summary>
         Error,
+        /// <summary>
+        /// Indicates that the server understood the request but refuses to authorize
+        /// </summary>
         Forbidden,
+        /// <summary>
+        /// Indicates that the request requires authorization
+        /// </summary>
         Unauthorized,
+        /// <summary>
+        /// Indicates that the request could not be understood by the server
+        /// </summary>
         Invalid,
+        /// <summary>
+        /// Indicates that the server did not find a current representation for the target resource
+        /// </summary>
         NotFound,
+        /// <summary>
+        /// Indicates that the request could not be completed due to a conflict with the current state of the target resource
+        /// </summary>
         Conflict,
+        /// <summary>
+        /// Indicates that the server is temporarily unavailable
+        /// </summary>
         Unavailable
     }
 }
