23d073866b9f5372fa006601569e6ab752bdb1d7
khai-nq <khai.nguyen.quang@gosell.vn>
2024-08-21 17:52:32 +0700

    chore: remove unuse

--- src/ApiGateways/Web.ApiGateway/Extensions/Extensions.cs
+++ src/ApiGateways/Web.ApiGateway/Extensions/Extensions.cs
@@ -19,11 +19,7 @@ namespace Web.ApiGateway.Extensions
         }
         public static IServiceCollection AddGrpcServices(this IServiceCollection services)
         {
-            services.AddGrpcClient<Employee.EmployeeClient>((services, options) =>
-            {
-                var employeeApi = services.GetRequiredService<IOptions<UrlsConfiguration>>().Value.GrpcEmployee;
-                options.Address = new Uri(employeeApi);
-            });
+            
 
             return services;
         }
